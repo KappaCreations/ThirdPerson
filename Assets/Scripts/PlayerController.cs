432356f12019bc21844403a2c5a3f78e0088d8f2
SirKornholio <54858728+SirKornholio@users.noreply.github.com>
2020-12-11 05:54:10 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    [SerializeField]
+    float moveSpeed = 3.0f;
+
+    [Header("References")]
+    [SerializeField]
+    Transform mainCamera;
+    [SerializeField]
+    BoxCollider swordCollider;
+    [SerializeField]
+    Portal portal1;
+    [SerializeField]
+    Portal portal2;
+    [SerializeField]
+    Camera mainCam;
+    Rigidbody rb;
+    Animator anim;
+    
+    private LineRenderer trail;
+
+
+    bool startedCombo = false;
+    float timeSinceButtonPressed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        var camForward = mainCamera.forward;
+        var camRight = mainCamera.right;
+
+        camForward.y = 0;
+        camForward.Normalize();
+        camRight.y = 0;
+        camRight.Normalize();
+
+        var moveDirection = (camForward * v * moveSpeed) + (camRight * h * moveSpeed);
+
+        transform.LookAt(transform.position + moveDirection);
+        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+
+        anim.SetFloat("moveSpeed", Mathf.Abs(moveDirection.magnitude));
+
+        if(Input.GetButtonDown("Jump") && !startedCombo)
+        {
+            anim.SetTrigger("swordCombo");
+            startedCombo = true;
+        }
+
+        if(Input.GetButtonDown("Jump") && startedCombo)
+        {
+            timeSinceButtonPressed = 0;
+        }
+
+        timeSinceButtonPressed += Time.deltaTime;
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                if(hit.collider.tag == "Environment" )
+                {
+                    Debug.Log("Hit");
+                    
+                    portal1.transform.position = hit.transform.position -  (new Vector3(0,0,hit.transform.localScale.z+0.2f) / 2);
+                    portal1.transform.localRotation = hit.transform.localRotation;
+                }
+            }
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Environment")
+                {
+                    Debug.Log("Hit");
+
+                    portal2.transform.position = hit.transform.position - (new Vector3(0, 0, hit.transform.localScale.z + 0.2f) / 2);
+                    portal2.transform.localRotation = hit.transform.localRotation;
+                }
+            }
+        }
+    }
+
+    public void PotentialComboEnd()
+    {
+        TurnOffSwordCollider();
+
+        if (timeSinceButtonPressed < 0.5f)
+            return;
+
+        anim.SetTrigger("stopCombo");
+        startedCombo = false;
+        timeSinceButtonPressed = 0;
+        
+    }
+
+    public void EndOfCombo()
+    {
+        startedCombo = false;
+        timeSinceButtonPressed = 0;
+        TurnOffSwordCollider();
+    }
+
+    public void TurnOnSwordCollider()
+    {
+        swordCollider.enabled = true;
+    }
+
+    public void TurnOffSwordCollider()
+    {
+        swordCollider.enabled = false;
+    }
+
+}
